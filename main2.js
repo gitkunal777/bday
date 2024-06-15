aaa7af506f2d4a2ec898619558eac8036d1ca98a
gitkunal777 <gitkunal777@gmail.com>
2024-06-15 10:53:11 +0000

    gfds

--- main2.js
+++ main2.js
@@ -1,62 +1,67 @@
 // script.js
 
-const buttons = document.querySelectorAll('.surprise-button');
-const messageArea = document.getElementById('message-area');
+const buttons = document.querySelectorAll(".surprise-button");
+const messageArea = document.getElementById("message-area");
 
 buttons.forEach((button) => {
-  button.addEventListener('click', () => {
-   let message = '';
-   switch (button.id) {
-    case 'button-1':
-     message = 'This website is from my side Happy Birthday!';
-     break;
-       
-    case 'button-2':
-     message = "Happy Birthday! wishing you a day filled with joy, laughter and unforgettable moments. May the year ahead bring you success, happiness and everything you've been hoping for. Enjoy your day to the fullest! Bhiya!";
-     break;
-       
-    case 'button-3':
-     message = 'Wishing you a fantastic birthday, Bhiya!';
-     break;
-       
-    case 'button-4':
-     message = "Happy Birthday, Pradeep bhaiya! Wishing you a fantastic day filled with joy and all your favorite things. Cheers to another great year ahead!";
-     break;
-       
-    case 'button-5':
-     message = "Wishing you a day filled with happiness and a year filled with joy. May your birthday be as incredible as you are! Another trip around the sun-time to celebrate! here's to another year of adventure, laughter and amazing memories. Sending you lots of love om your special day! Happy Birthday!";
-     break;
-       
-    case 'button-6':
-     message = "Happy Birthday to my love of life. I am so gratefull for your love and support everyday. Here's to anotrher year to making memories together.";
-     break;
-       
-    case 'button-7':
-     message = "Dear Pradeep, today is your birthday. I wish this day will come again and again in your life. Your silent attitude should stay with you everytime. You should go very successful I wish and this is all from my side.";
-     break;
-       
-    case 'button-8':
-     message = "Bhaw bhaw bhawwwwwwwwww Happy Birthday.";
-     break;
+  button.addEventListener("click", () => {
+    let message = "";
+    switch (button.id) {
+      case "button-1":
+        message = "This website is from my side Happy Birthday!";
+        break;
 
-    case 'button-9':
-     message = "Wishing you happiest birthday mama enjoy your birthday.";
-     break;
+      case "button-2":
+        message =
+          "Happy Birthday! wishing you a day filled with joy, laughter and unforgettable moments. May the year ahead bring you success, happiness and everything you've been hoping for. Enjoy your day to the fullest! Bhiya!";
+        break;
 
-    case 'button-10':
-     message = "Happy Birthday mama many many returns of the day.";
-     break;
-   }
-   messageArea.value = message;
+      case "button-3":
+        message = "Wishing you a fantastic birthday, Bhiya!";
+        break;
+
+      case "button-4":
+        message =
+          "Happy Birthday, Pradeep bhaiya! Wishing you a fantastic day filled with joy and all your favorite things. Cheers to another great year ahead!";
+        break;
+
+      case "button-5":
+        message =
+          "Wishing you a day filled with happiness and a year filled with joy. May your birthday be as incredible as you are! Another trip around the sun-time to celebrate! here's to another year of adventure, laughter and amazing memories. Sending you lots of love om your special day! Happy Birthday!";
+        break;
+
+      case "button-6":
+        message =
+          "Happy Birthday to my love of life. I am so gratefull for your love and support everyday. Here's to anotrher year to making memories together.";
+        break;
+
+      case "button-7":
+        message =
+          "Dear Pradeep, today is your birthday. I wish this day will come again and again in your life. Your silent attitude should stay with you everytime. You should go very successful I wish and this is all from my side.";
+        break;
+
+      case "button-8":
+        message = "Bhaw bhaw bhawwwwwwwwww Happy Birthday.";
+        break;
+
+      case "button-9":
+        message = "Wishing you happiest birthday mama enjoy your birthday.";
+        break;
+
+      case "button-10":
+        message = "Happy Birthday mama many many returns of the day.";
+        break;
+    }
+    messageArea.value = message;
   });
 });
 
-const nextButton = document.querySelector('.next-button');
+const nextButton = document.querySelector(".next-button");
 
-nextButton.addEventListener('click', () => {
+nextButton.addEventListener("click", () => {
   // Simulate a page transition
   setTimeout(() => {
-   // Transition to the next page (button.html)
-   window.location.href = 'last.html';
+    // Transition to the next page (button.html)
+    window.location.href = "last.html";
   }, 500);
 });
--- main3.js
+++ main3.js
@@ -1,14 +1,14 @@
-const candles = document.querySelectorAll('.candle');
-const blowButton = document.getElementById('blow-button');
-const wishMessage = document.getElementById('wish-message');
+const candles = document.querySelectorAll(".candle");
+const blowButton = document.getElementById("blow-button");
+const wishMessage = document.getElementById("wish-message");
 
-blowButton.addEventListener('click', () => {
+blowButton.addEventListener("click", () => {
   candles.forEach((candle) => {
-   candle.classList.add('blow');
+    candle.classList.add("blow");
   });
   setTimeout(() => {
-   wishMessage.classList.add('show');
+    wishMessage.classList.add("show");
   }, 1000);
-  var audio = new Audio('song.mp3');
+  var audio = new Audio("song.mp3");
   audio.play();
 });
